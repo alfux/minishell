87b7fe0d203789516bdf0ce2e898a3c6ebd6906c
alfux <alexis.t.fuchs@gmail.com>
2022-08-21 22:24:12 +0200

    norm

--- src/ft_execute.c
+++ src/ft_execute.c
@@ -6,7 +6,7 @@
 /*   By: alfux <alexis.t.fuchs@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 19:48:07 by alfux             #+#    #+#             */
-/*   Updated: 2022/08/20 17:04:08 by alfux            ###   ########.fr       */
+/*   Updated: 2022/08/21 22:23:45 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -15,8 +15,6 @@ void	ft_execute(char **cmd, char ***ev, char ***var)
 {
 	if (ft_isbuiltin(cmd, ev, var))
 		return ;
-//	else if (ft_isexternal(cmd, ev, var))
-//		return ;
 	else
 		ft_errmsg(-4);
 }
